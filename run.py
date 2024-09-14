551a9fa513ab802b151d91b177199319ea966f8d
pQbas <percybrayan18@gmail.com>
2024-09-14 09:33:54 -0500

    changed name from run to demo

--- run.py
+++
@@ -1,9 +0,0 @@
-from alexnet.train import train, getConfig
-
-# Training in a given dataset
-
-CONFIG_FILE_PATH = '/home/pqbas/projects/alexnet-pytorch/alexnet/config.ini'
-
-config = getConfig(CONFIG_FILE_PATH)
-
-train(params = config)
