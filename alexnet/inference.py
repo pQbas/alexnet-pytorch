742ef4d2e3a6b4a562f02f4f9b411382d735224f
pQbas <percybrayan18@gmail.com>
2024-09-14 10:01:32 -0500

    Added inference file with main structure

---
+++ alexnet/inference.py
@@ -0,0 +1,38 @@
+from alexnet.model import AlexNet
+
+def loadModel(weights):
+    '''
+    Loads the weights path to the model, and
+    return the model
+    '''
+
+    return model
+
+
+def preprocess(inputData):
+    '''
+    Apply preproceesing to the Data
+    '''
+
+    return preprocessData
+
+
+def predict(preprocessedData):
+    '''
+    Perform prediction over the preprocessedData
+    '''
+
+    return prediction
+
+
+def postprocess(prediction):
+    '''
+    Apply post processing over prediction to make
+    the probabilities and convert it into a json
+    file with ineterpretable predictions
+    '''
+
+    return postProcessPrediction
+
+
+
