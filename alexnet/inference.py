f6a3e193341fe7df82f9ef12eb996fd01136c568
pQbas <percybrayan18@gmail.com>
2024-09-15 08:18:34 -0500

    Main structure at inference file was set

--- alexnet/inference.py
+++ alexnet/inference.py
@@ -1,10 +1,12 @@
 from alexnet.model import AlexNet
 
-def loadModel(weights):
+def loadModel(weightsPath):
     '''
     Loads the weights path to the model, and
     return the model
     '''
+
+    model = load...
 
     return model
 
@@ -17,13 +19,6 @@ def preprocess(inputData):
     return preprocessData
 
 
-def predict(preprocessedData):
-    '''
-    Perform prediction over the preprocessedData
-    '''
-
-    return prediction
-
 
 def postprocess(prediction):
     '''
@@ -35,4 +30,22 @@ def postprocess(prediction):
     return postProcessPrediction
 
 
+def predict(preprocessedData):
+    '''
+    Perform prediction over the preprocessedData
+    '''
 
+    return prediction
+
+
+def inference(input, weightsPath):
+
+    model = loadModel(weightsPath)
+
+    preprocessData = preprocess(input)
+
+    prediction = predict(preprocessData)
+
+    postprocessData = postprocess(prediction)
+
+    return postprocessData
