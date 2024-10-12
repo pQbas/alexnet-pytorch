56a1bc9155af7864098c09d91fb76ee91b94a3d1
pQbas <percybrayan18@gmail.com>
2024-10-12 16:27:53 -0500

    setup was update

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
+import os
 from setuptools import setup, find_packages
 
 # Function to read the requirements from requirements.txt
 def parse_requirements(filename):
     """Load requirements from a pip requirements file."""
-    with open(filename, 'r') as req_file:
+    with open(os.path.join(os.path.dirname(__file__), filename), 'r') as req_file:
         return req_file.read().splitlines()
 
 # Short description of your project
@@ -27,7 +28,5 @@ setup(
     python_requires='>=3.9',  # Specify the minimum Python version required
     
     # Dynamically load install_requires from requirements.txt
-    install_requires=parse_requirements('./requirements.txt'),
+    install_requires=parse_requirements('requirements.txt'),
 )
-
-
