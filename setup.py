96b06c5cce103372c70dc7708cde12e68a55ad39
pQbas <percybrayan18@gmail.com>
2024-10-12 16:03:32 -0500

    added requirements.txt file to the setup.py file

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup, find_packages
 
-description_ = """Library that gives an abstraction layer to work with Alexnet CNN wit"""
+# Function to read the requirements from requirements.txt
+def parse_requirements(filename):
+    """Load requirements from a pip requirements file."""
+    with open(filename, 'r') as req_file:
+        return req_file.read().splitlines()
+
+# Short description of your project
+description_ = """Library that gives an abstraction layer to work with Alexnet CNN"""
 
 setup(
     name='alexnet',  # Name of your library
     version='0.1.0',  # Initial version number
     author='Percy Cubas',  # Your name
-    author_email ='pcubasm1@gmail.com',  # Your email address
-    description = description_,  # Short description
+    author_email='pcubasm1@gmail.com',  # Your email address
+    description=description_,  # Short description
     long_description=open('README.md').read(),  # Long description from README
     long_description_content_type='text/markdown',  # Format of long description
     url='https://github.com/pQbas/alexnet-pytorch.git',  # URL of your project's repository
@@ -18,13 +25,9 @@ setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.9',  # Specify the minimum Python version required
-    install_requires=[
-        'torch==2.2.1',
-        'torchvision==0.17.1',
-        'numpy',
-        'tqdm',
-        'colorlog',
-        'rich'
-    ],
+    
+    # Dynamically load install_requires from requirements.txt
+    install_requires=parse_requirements('requirements.txt'),
 )
+
 
