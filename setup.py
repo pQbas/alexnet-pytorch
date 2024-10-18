cb5f4675903497b0c5bda8674a989a2d12a70388
pQbas <percybrayan18@gmail.com>
2024-10-18 12:15:46 -0500

    new version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ description_ = """Library that gives an abstraction layer to work with Alexnet C
 
 setup(
     name='alexnet',  # Name of your library
-    version='0.1.6',  # Initial version number
+    version='0.1.7',  # Initial version number
     author='Percy Cubas',  # Your name
     author_email='pcubasm1@gmail.com',  # Your email address
     description=description_,  # Short description
